ce790544fa6ff17e46b16567b3c9459c29987003
Wendel Lisboa <163331710+Lisboon@users.noreply.github.com>
2025-07-11 15:37:45 -0400

    Fix Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors;
-  
-  const config = new DocumentBuilder()
-    .setTitle('MeusLeads API')
-    .setDescription('API para o Fullstack Challenge')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
   
   await app.listen(process.env.PORT || 3000);
   
